8ce4d9d5bbd543f468f1f250eaa7c5d6b7a7fb5a
xamiro <baumgart.guenter02@gmail.com>
2019-08-26 17:57:08 +0200

    Adding cleanup temperature sensor

--- auto-reverse/temperature.h
+++ auto-reverse/temperature.h
@@ -8,16 +8,12 @@ MAX6675 ktc(TEMPERTURE_SCK_PIN, TEMPERTURE_CS_PIN, TEMPERTURE_SO);
 
 int temperature = 0;
 unsigned long temperature_TS = 0;
-void temperatur_setup() {
-  temperature_TS = millis();
-}
 
 void temperature_loop() {
 
   if (millis() - temperature_TS > TEMPERTURE_INTERVAL) {
     temperature_TS = millis();
     temperature = ktc.readCelsius();
-    Serial.println(temperature);
   }
 
 }
